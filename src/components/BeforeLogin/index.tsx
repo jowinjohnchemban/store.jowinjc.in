cc7ac67c0bac2fa18fa20fbb3c583a7fa4e843fa
jowinjohnchemban <jowinchemban@gmail.com>
2025-11-12 01:08:57 +0530

    fix: update login URL to use SERVER_URL variable

--- src/components/BeforeLogin/index.tsx
+++ src/components/BeforeLogin/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 export const BeforeLogin: React.FC = () => {
+
+  const SERVER_URL =
+    process.env.NEXT_PUBLIC_SERVER_URL ||
+    process.env.PAYLOAD_PUBLIC_SERVER_URL ||
+    undefined;
+
+  const loginURL = SERVER_URL ? `${SERVER_URL}/login` : `/login`;
+
   return (
     <div>
       <p>
         <b>Welcome to your dashboard!</b>
         {' This is where site admins will log in to manage your store. Customers will need to '}
-        <a href={`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/login`}>log in to the site instead</a>
+        <a href={loginURL}>log in to the site instead</a>
         {' to access their user account, order history, and more.'}
       </p>
     </div>
